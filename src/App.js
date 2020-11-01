78d99782423a1e888a7e4e70d255e370e559f1db
TRAN QUANG CHIEN <qctran1991@gmail.com>
2020-11-01 22:45:23 +0100

    add api structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { BrowserRouter, Switch, Redirect, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import NotFound from './components/NotFound';
 import Header from './components/Header';
+import productApi from 'api/productApi';
 
 const Photo = React.lazy(() => import('./features/Photo'));
 
 function App() {
+  // save on state, or save on redux ....
+  const [productList, setProductList ] = useState([]);
+
+  useEffect(() => {
+    const fetchProductList = async () => {
+      try{
+        const params = {
+          _page : 1,
+          _limit : 10
+        }
+        const response = await productApi.getAll(params);
+        setProductList(response.data);
+        console.log(response);
+      }catch(e){
+        console.log("Failed to fetch products list ", e);
+      }
+    }
+    fetchProductList();
+  }, []);
+
   return (
     <div className="photo-app">
       <Suspense fallback={<div>Loadding ...</div>}>
